f64486458d113c3f59dcef2f3f34f8cdddfbc6f6
Phil Lee <prodigy7748g@gmail.com>
2021-02-25 11:38:02 +0800

    done test visit task index page

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.2'
   gem 'faker', '~> 2.16.0'
+  gem "factory_bot_rails"
 end
 
 group :development do
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  
+
+  describe 'visit task index page' do
+    it 'should be able to see all tasks' do
+      3.times do 
+        create(:task)
+      end
+
+      visit tasks_path
+
+      titles = all('#task_index_table tr > td:first-child').map{|t| t.text}
+      #可改寫成.map(&:text)
+      result = Task.all.map{|t| t.title}
+      expect(titles).to eq result
+    end
+  end
 end
