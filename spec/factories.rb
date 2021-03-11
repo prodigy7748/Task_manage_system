9fb5ab70b8a1c2b9b6e541742a286e3f74a045e2
Phil Lee <prodigy7748g@gmail.com>
2021-03-11 15:55:41 +0800

    add task create test with diff senerios

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
     title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
     start_time { Faker::Time.between(from: DateTime.now - 3.day ,to: DateTime.now - 2.day) }
     end_time { Faker::Time.between(from: DateTime.now - 1.day, to: DateTime.now) }
     priority { 1 }
     status { 1 }
-    content { Faker::Lorem.paragraph }
   end
 end
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,19 +1,60 @@
 require 'rails_helper'
 
-RSpec.feature "Tasks", type: :feature do
+RSpec.feature 'Tasks', type: :feature do
+  let(:title) { Faker::Lorem.sentence }
+  let(:content) { Faker::Lorem.paragraph }
 
-  describe 'visit task index page' do
-    it 'should be able to see all tasks' do
+  describe 'user visit task index page' do
+    senerio 'tasks show on index page' do
       3.times do 
-        create(:task)
+        FactoryBot.create(:task)
       end
-
       visit tasks_path
 
-      titles = all('#task_index_table tr > td:first-child').map{|t| t.text}
-      #可改寫成.map(&:text)
+      titles = all('#task_index_table tr > td:first-child').map(&:text)
       result = Task.all.map{|t| t.title}
       expect(titles).to eq result
     end
   end
+
+  describe 'user creates a new task' do
+    scenario 'with title and content' do
+      visit new_task_path
+      fill_in 'Title', with: title
+      fill_in 'Content', with: content
+      click_button 'Create Task'
+
+      expect(Task.all.size).to eq 1
+      expect(page).to have_content('新增任務成功！')
+      expect(page).to have_content(title)
+      expect(page).to have_content(content)
+    end
+
+    scenario 'without title and content' do
+      visit new_task_path
+      fill_in 'Title', with: ''
+      fill_in 'Content', with: ''
+      click_button 'Create Task'
+
+      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content('Content can\'t be blank')
+    end
+
+    scenario 'without title' do
+      visit new_task_path
+      fill_in 'Title', with: ''
+      click_button 'Create Task'
+
+      expect(page).to have_content('Title can\'t be blank')
+    end
+
+    scenario 'without content' do
+      visit new_task_path
+      fill_in 'Content', with: ''
+      click_button 'Create Task'
+
+      expect(page).to have_content('Content can\'t be blank')
+    end
+  end
+
 end
