809c3dcfee096f5ef978c81a38be013570207818
Phil Lee <prodigy7748g@gmail.com>
2021-03-11 17:48:38 +0800

    add task edit/update test

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -43,12 +43,40 @@ RSpec.feature 'Tasks', type: :feature do
   end
 
   describe 'show a task' do
-    it do
+    it 'should show right content of a task' do
       task = Task.create(title: title, content: content)
       visit task_path(task)
 
       expect(page).to have_content(title)
       expect(page).to have_content(content)
+    end
+  end
+
+  describe 'edit a task' do
+    let(:task) { create(:task) }
+    let(:new_title) { Faker::Lorem.sentence }
+    let(:new_content) { Faker::Lorem.paragraph }
+
+    it 'with new_title and new_content' do
+      edit_task(new_title, new_content)
+      expect(page).to have_content(new_title)
+      expect(page).to have_content(new_content)
+    end
+
+    it 'without title and content' do
+      edit_task()
+      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content('Content can\'t be blank')
+    end
+
+    it 'without title' do
+      edit_task(nil, new_content)
+      expect(page).to have_content('Title can\'t be blank')
+    end
+
+    it 'without title' do
+      edit_task(new_title ,nil)
+      expect(page).to have_content('Content can\'t be blank')
     end
   end
 
@@ -58,6 +86,13 @@ RSpec.feature 'Tasks', type: :feature do
     fill_in '任務名稱', with: title
     fill_in '內容', with: content
     click_button '新增任務'
+  end
+
+  def edit_task(new_title = nil, new_content = nil)
+    visit edit_task_path(task)
+    fill_in '任務名稱', with: new_title
+    fill_in '內容', with: new_content
+    click_button '更新任務'
   end
 
 end
